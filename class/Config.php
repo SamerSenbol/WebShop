d0f7128f6cd91eb351ffe271bff9a0a2aa93e27f
SamerSenbol <samer.senbol@gmail.com>
2021-09-07 17:26:02 +0200

    Update Config.php

--- class/Config.php
+++ class/Config.php
@@ -1,8 +1,13 @@
 <?php
 
-define('DB_NAME', 'webshop');
+/* define('DB_NAME', 'webshop');
 define('DB_USER', 'root');
 define('DB_PASSWORD', '');
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'localhost'); */
+
+define('DB_NAME', 'heroku_fc58b73805405c5');
+define('DB_USER', 'bdc8b686703f7c');
+define('DB_PASSWORD', '1fdfb1e8');
+define('DB_HOST', 'eu-cdbr-west-01.cleardb.com');
 
 ?>
